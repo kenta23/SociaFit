ba4b1bf393bc181b9243cdeaaa548b3b71cf96e4
kenta23 <dreyx0159@gmail.com>
2025-10-22 21:44:06 +0800

    suppressed lint on useEffect

--- app/profile-visit/[userid].tsx
+++ app/profile-visit/[userid].tsx
@@ -1,218 +1,245 @@
-import ActivityContent from '@/components/feeds/activities';
-import { Colors } from '@/constants/Colors';
-import { typography } from '@/constants/typography';
-import { Database } from "@/database.types";
+import ActivityContent from "@/components/feeds/activities";
+import { Colors } from "@/constants/Colors";
+import { typography } from "@/constants/typography";
+import type { Database } from "@/database.types";
 import { getAuthUser, getUserData } from "@/utils/auth";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { AuthResponse } from "@supabase/supabase-js";
-import { Image } from 'expo-image';
+import Ionicons from "@expo/vector-icons/Ionicons";
+import type { AuthResponse } from "@supabase/supabase-js";
+import { Image } from "expo-image";
 import { Redirect, useLocalSearchParams, useRouter } from "expo-router";
 import React, { useEffect, useState } from "react";
-import { Pressable, ScrollView, StyleSheet, Text, useColorScheme, View } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
+import {
+    Pressable,
+    ScrollView,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-
-interface UserDataProps { 
-    authSession: AuthResponse['data']['user'] | null;
-    following: Database['public']['Tables']['following']['Row'][] | null;
-    totalLikes: number;
-    activities: Database['public']['Tables']['activities']['Row'][] | null;
-    totalActivities: number;
+interface UserDataProps {
+	authSession: AuthResponse["data"]["user"] | null;
+	following: Database["public"]["Tables"]["following"]["Row"][] | null;
+	totalLikes: number;
+	activities: Database["public"]["Tables"]["activities"]["Row"][] | null;
+	totalActivities: number;
 }
-
 
 export default function ProfileVisit() {
-    const { userid } = useLocalSearchParams();
-    const colorScheme = useColorScheme() ?? 'light';
-    const router = useRouter();
-    const [userdata, setUserdata] = useState<UserDataProps[]>([{ 
-        authSession: null,
-        following: [],
-        totalLikes: 0,
-        activities: [],
-        totalActivities: 0,
-    }])
+	const { userid } = useLocalSearchParams();
+	const colorScheme = useColorScheme() ?? "light";
+	const router = useRouter();
+	const [userdata, setUserdata] = useState<UserDataProps[]>([
+		{
+			authSession: null,
+			following: [],
+			totalLikes: 0,
+			activities: [],
+			totalActivities: 0,
+		},
+	]);
 
+	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+	useEffect(() => {
+		const getUser = async () => {
+			const user = await getAuthUser();
 
-    useEffect(() => { 
-         const getUser = async () => { 
+			if (!user.data.user) {
+				return <Redirect href="/login" />;
+			}
+			if (userid === user.data.user?.id) {
+				return <Redirect href="/profile" />;
+			}
+			const { userData, following, likes, activities } = await getUserData(
+				userid as string,
+			);
 
-              const user = await getAuthUser();
-            
-              if (!user.data.user) { 
-                return <Redirect href="/login" />
-              }
-              if (userid === user.data.user?.id) {
-                return <Redirect href="/profile" />
-              }
-              const { userData, following, likes, activities } = await getUserData(userid as string);
+			setUserdata([
+				{
+					authSession: user.data.user,
+					following: following,
+					totalLikes: likes?.length || 0,
+					activities: activities,
+					totalActivities: activities?.length || 0,
+				},
+			]);
+		};
 
-    
-              setUserdata([{
-                authSession: user.data.user,
-                following: following,
-                totalLikes: likes?.length || 0,
-                activities: activities,
-                totalActivities: activities?.length || 0,
-              }]);
-             
-         }
+		getUser();
+	}, []);
 
-         getUser();
-    }, [])
+	return (
+		<SafeAreaView edges={["bottom"]} style={{ flex: 1 }}>
+			<ScrollView style={styles.container}>
+				{/**QR CODE SCANNER */}
+				<Pressable
+					onPress={() => router.push("/my-qr-code")}
+					style={styles.qrCodeScanner}
+				>
+					<Ionicons name="qr-code-outline" size={28} color="#24B437" />
+				</Pressable>
 
+				{/**Your Profile View */}
+				<View style={styles.parent}>
+					<View style={styles.view}>
+						<View style={[styles.frameParent, styles.frameFlexBox]}>
+							<Image
+								style={styles.frameChild}
+								contentFit="cover"
+								source={require("@/assets/images/no-user.png")}
+							/>
 
- return (
-    <SafeAreaView edges={['bottom']} style={{ flex: 1 }}>
-        <ScrollView style={styles.container}>  
-            {/**QR CODE SCANNER */}
-            <Pressable onPress={() => router.push('/my-qr-code')} style={styles.qrCodeScanner}>
-              <Ionicons name='qr-code-outline' size={28} color='#24B437' />
-            </Pressable>
+							<View style={styles.nameContainer}>
+								<Text style={[typography.heading]}>Unknown User</Text>
+								<Text style={typography.medium}>
+									{userdata[0].authSession?.email}
+								</Text>
+							</View>
+						</View>
 
-          {/**Your Profile View */}
-          <View style={styles.parent}>
-              <View style={styles.view}>
+						<View style={[styles.frameGroup, styles.frameFlexBox]}>
+							<View style={[styles.groupFlexBox]}>
+								<Text style={[typography.medium]}>
+									{userdata[0].totalLikes}
+								</Text>
+								<Text style={[typography.description]}>Total likes</Text>
+							</View>
 
-                  <View style={[styles.frameParent, styles.frameFlexBox]}>
-                      <Image style={styles.frameChild} contentFit="cover" source={require('@/assets/images/no-user.png')} />
-               
-                      <View style={styles.nameContainer}>
-                          <Text style={[typography.heading]}>Unknown User</Text>
-                          <Text style={typography.medium}>{userdata[0].authSession?.email}</Text>
-                      </View>
-                  </View>
+							<View style={[styles.groupFlexBox]}>
+								<Text style={[typography.medium]}>
+									{userdata[0].totalActivities}
+								</Text>
+								<Text style={[typography.description]}>Activities</Text>
+							</View>
 
+							<View style={styles.groupFlexBox}>
+								<Text style={[typography.medium]}>
+									{userdata[0].following?.length || 0}
+								</Text>
+								<Text style={[typography.description]}>Followers</Text>
+							</View>
+						</View>
+					</View>
+				</View>
 
-                  <View style={[styles.frameGroup, styles.frameFlexBox]}>
-                      <View style={[styles.groupFlexBox]}>
-                          <Text style={[typography.medium]}>{userdata[0].totalLikes}</Text>
-                          <Text style={[typography.description]}>Total likes</Text>
-                      </View>
+				{/** Horizontal Line */}
+				<View
+					style={{
+						height: 1,
+						backgroundColor: Colors[colorScheme].text["50"],
+						width: "100%",
+						marginVertical: 10,
+					}}
+				/>
 
-                      <View style={[styles.groupFlexBox]}>
-                          <Text style={[typography.medium]}>{userdata[0].totalActivities}</Text>
-                          <Text style={[typography.description]}>Activities</Text>
-                      </View>
+				{/**YOUR ACTIVITIES */}
+				<View style={styles.activitiesContainer}>
+					<Text style={typography.subheading}>Your Activities</Text>
+					<ActivityContent
+						userid={
+							userdata[0].activities?.filter(
+								(activity) => activity.user_id === userid,
+							)?.[0]?.user_id ?? ""
+						}
+					/>
+				</View>
 
-                      <View style={styles.groupFlexBox}>
-                          <Text style={[typography.medium]}>{userdata[0].following?.length || 0}</Text>
-                          <Text style={[typography.description]}>Followers</Text>
-                      </View>
-
-                  </View>
-              </View>
-          </View>
-
-            {/** Horizontal Line */}
-       <View style={{ height: 1, backgroundColor: Colors[colorScheme].text['50'], width: '100%', marginVertical: 10 }} />
-
-            {/**YOUR ACTIVITIES */}
-          <View style={styles.activitiesContainer}>
-              <Text style={typography.subheading}>Your Activities</Text>
-              <ActivityContent userid={
-                userdata[0].activities?.filter((activity) => activity.user_id === userid)?.[0]?.user_id ?? ''
-               }/>
-          </View>
-          
-           {/* Add bottom padding for better scrolling experience */}
-          <View style={{ height: 50 }} />
-       </ScrollView>
-   </SafeAreaView>
-    )
+				{/* Add bottom padding for better scrolling experience */}
+				<View style={{ height: 50 }} />
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
-
-
 const styles = StyleSheet.create({
-    container: { 
-        flex: 1,
-        width: '100%',
-        paddingHorizontal: 24,
-      },
-    activitiesContainer: { 
-         marginTop: 12,
-         gap: 10,
-    },
-    qrCodeScanner: {
-        position: 'absolute',
-        top: 10,
-        right: 10,
-        zIndex: 10,
-        padding: 10,
-        borderRadius: 10,
-        marginBottom: 12,
-        width: 'auto',
-        height: 60,
-        justifyContent: 'center',
-    },
-    /**Your Profile View Styles */
-    parent: {
-        marginTop: 25,
-        paddingVertical: 12,
-        height: 'auto',
-    },
-    frameFlexBox: {
-        alignItems: "center",
-        flexDirection: "row"
-    },
-    groupFlexBox: {
-        gap: 4,
-        alignItems: "center"
-    },
-    frameChild: {
-        width: 75,
-        borderRadius: 100,
-        height: 75,
-        overflow: "hidden"
-    },
-    myName: {
-        fontSize: 18,
-        fontFamily: "Inter_400Regular",
-        textAlign: "center"
-    },
-    username: {
-        fontSize: 16,
-        fontWeight: "500",
-        fontFamily: "Inter_500Medium",
-        textAlign: "left",
-        alignSelf: "stretch"
-    },
-    nameContainer: {
-        width: 'auto',
-        gap: 7,
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        justifyContent: 'center',
-    },
-    frameParent: {
-        gap: 13,
-        alignSelf: "stretch"
-    },
-    values: {
-        fontSize: 20,
-        fontWeight: "600",
-        fontFamily: "Inter_600SemiBold"
-    },
-    description: {
-        fontSize: 12,
-        fontFamily: "Inter_400Regular"
-    },
+	container: {
+		flex: 1,
+		width: "100%",
+		paddingHorizontal: 24,
+	},
+	activitiesContainer: {
+		marginTop: 12,
+		gap: 10,
+	},
+	qrCodeScanner: {
+		position: "absolute",
+		top: 10,
+		right: 10,
+		zIndex: 10,
+		padding: 10,
+		borderRadius: 10,
+		marginBottom: 12,
+		width: "auto",
+		height: 60,
+		justifyContent: "center",
+	},
+	/**Your Profile View Styles */
+	parent: {
+		marginTop: 25,
+		paddingVertical: 12,
+		height: "auto",
+	},
+	frameFlexBox: {
+		alignItems: "center",
+		flexDirection: "row",
+	},
+	groupFlexBox: {
+		gap: 4,
+		alignItems: "center",
+	},
+	frameChild: {
+		width: 75,
+		borderRadius: 100,
+		height: 75,
+		overflow: "hidden",
+	},
+	myName: {
+		fontSize: 18,
+		fontFamily: "Inter_400Regular",
+		textAlign: "center",
+	},
+	username: {
+		fontSize: 16,
+		fontWeight: "500",
+		fontFamily: "Inter_500Medium",
+		textAlign: "left",
+		alignSelf: "stretch",
+	},
+	nameContainer: {
+		width: "auto",
+		gap: 7,
+		flexDirection: "column",
+		alignItems: "flex-start",
+		justifyContent: "center",
+	},
+	frameParent: {
+		gap: 13,
+		alignSelf: "stretch",
+	},
+	values: {
+		fontSize: 20,
+		fontWeight: "600",
+		fontFamily: "Inter_600SemiBold",
+	},
+	description: {
+		fontSize: 12,
+		fontFamily: "Inter_400Regular",
+	},
 
-    followers: {
-        width: 61,
-        height: 15
-    },
-    frameGroup: {
-        gap: 25,
-        justifyContent: "center"
-    },
-    view: {
-        width: "auto",
-        alignItems: "center",
-        gap: 15,
-        justifyContent: "center",
-        flexDirection: 'column',
-    }
-
-})
+	followers: {
+		width: 61,
+		height: 15,
+	},
+	frameGroup: {
+		gap: 25,
+		justifyContent: "center",
+	},
+	view: {
+		width: "auto",
+		alignItems: "center",
+		gap: 15,
+		justifyContent: "center",
+		flexDirection: "column",
+	},
+});
